2a775e4e27e78ea7b7807a05c62078012b45834e
DarkenedDragon <eridor314@gmail.com>
2020-12-02 17:02:36 -0500

    Created PasswordLine struct

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,3 +1,31 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("day_2/input2.txt").expect("Failed to read input file");
+
+    // Split into each line
+    let contents = input.split("\n").collect::<Vec<&str>>();
+
+
+}
+
+struct PasswordLine<'a> {
+    range: (u32, u32),
+    character: char,
+    password: &'a str
+}
+
+impl<'a> PasswordLine<'a> {
+    fn new(line: &str) -> PasswordLine {
+        // should have three parts, the range, the letter, and the password
+        let parts = line.split_ascii_whitespace().collect::<Vec<&str>>();
+        let mut range = parts[0].split("-");
+        let low = range.next().unwrap().parse::<u32>().expect("Couldn't parse lower bound");
+        let high = range.next().unwrap().parse::<u32>().expect("Couldn't parse upper bound");
+
+        let letter = parts[1].chars().next().expect("Couldn't parse letter"); // get the first char
+        let password = parts[2];
+
+        return PasswordLine {range: (low, high), character: letter, password};
+    }
 }
