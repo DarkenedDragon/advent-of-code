df53ba999f799149b260353ba506fa2f3033079c
DarkenedDragon <eridor314@gmail.com>
2020-12-03 15:59:03 -0500

    Solution to part 1

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,3 +1,36 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let right = 3;
+    let down = 1;
+
+    let input = File::open("input3.txt").expect("Failed to read from file");
+    let reader = BufReader::new(input);
+
+    let mut trees = 0;
+    let mut pos = 0;
+
+    for line in reader.lines().step_by(down) {
+        match line {
+            Ok(t) => {
+                let len = t.chars().count();
+                let thing = match t.chars().nth(pos as usize) {
+                    Some(t) => t,
+                    None => '\0'
+                };
+
+                // if we've hit a tree
+                // the very first position should never be a tree
+                if thing == '#' {
+                    trees += 1;
+                }
+
+                pos = (pos + right) % len;
+            }
+            Err(_) => (),
+        };
+    }
+
+    println!("There are {} trees on this slope", trees);
 }
