b1fae1d9d8abfc4c01cbc68fd2ef1da998183281
DarkenedDragon <eridor314@gmail.com>
2020-12-03 16:12:02 -0500

    Modified to account for part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,36 +1,38 @@
-use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::fs;
 
 fn main() {
-    let right = 3;
-    let down = 1;
+    let input = fs::read_to_string("input3.txt").expect("Failed to read from file");
 
-    let input = File::open("input3.txt").expect("Failed to read from file");
-    let reader = BufReader::new(input);
+    // (Right, Down)
+    let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let mut runs : Vec<u32> = Vec::new();
 
-    let mut trees = 0;
-    let mut pos = 0;
+    for slope in slopes {
+        let mut trees = 0;
+        let mut pos = 0;
+        // every slope is repeating, so we can use modulo to save space
+        for line in input.lines().step_by(slope.1) {
+            let len = line.chars().count();
+            let thing = match line.chars().nth(pos as usize) {
+                Some(t) => t,
+                None => '\0'
+            };
 
-    for line in reader.lines().step_by(down) {
-        match line {
-            Ok(t) => {
-                let len = t.chars().count();
-                let thing = match t.chars().nth(pos as usize) {
-                    Some(t) => t,
-                    None => '\0'
-                };
-
-                // if we've hit a tree
-                // the very first position should never be a tree
-                if thing == '#' {
-                    trees += 1;
-                }
-
-                pos = (pos + right) % len;
+            // if we've hit a tree
+            // the very first position should never be a tree
+            if thing == '#' {
+                trees += 1;
             }
-            Err(_) => (),
-        };
+
+            pos = (pos + slope.0) % len;
+        }
+        println!("There are {} trees on slope Right: {}, Down: {}", trees, slope.0, slope.1);
+        runs.push(trees);
     }
 
-    println!("There are {} trees on this slope", trees);
+    let mut product: u32 = 1;
+    for run in runs {
+        product *= run;
+    }
+    println!("Their product is {}", product);
 }
