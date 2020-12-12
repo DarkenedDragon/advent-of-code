fe003896ca3830501dab4bff7658eedae3f5922f
DarkenedDragon <eridor314@gmail.com>
2020-12-12 18:59:44 -0500

    Added basic graph functionality

---
+++ day_7/src/lib.rs
@@ -0,0 +1,91 @@
+
+// stores an adjaceny list of nodes
+// This is a directed graph
+pub mod graph {
+    use std::collections::{HashMap};
+    use std::collections::hash_map::Entry;
+
+    #[derive(Debug)]
+    pub struct Graph<'a> {
+        adj_list: HashMap<&'a str, Vec<&'a str>>
+    }
+
+    impl<'a> Graph<'a> {
+        pub fn new() -> Graph<'a> {
+            let adj_list: HashMap<&'a str, Vec<&'a str>> = HashMap::new();
+            return Graph {adj_list};
+        }
+
+        // Inserts a node if not currently present. Does nothing with edges
+        pub fn insert_node(&mut self, node: &'a str) -> bool{
+            let mut out = false;
+            if !self.adj_list.contains_key(node) {
+                out = match self.adj_list.insert(node, Vec::new()) {
+                    Some(_t) => false,
+                    None => true
+                };
+            }
+
+            return out;
+        }
+
+        // Places an edge between two nodes. If one or both nodes are not present in the graph,
+        // they will be added
+        // This is a directed graph, so the node is placed node1 -> node2
+        // Returns false if the edge was already present or could not be added
+        pub fn insert_edge(&mut self, node1: &'a str, node2: &'a str) -> bool {
+            let mut out = false;
+            // Add node2 to the graph if not already present
+            if !self.adj_list.contains_key(node2) {
+                out = match self.adj_list.insert(node2, Vec::new()) {
+                    Some(_t) => false,
+                    None => true
+                };
+            }
+            // Add node2 to node1's list
+            // Will add node1 if not present
+            let node = self.adj_list.entry(node1).or_insert(Vec::new());
+            if !node.contains(&node2) {
+                node.push(node2);
+                out = true;
+            }
+
+            return out;
+        }
+
+        // Removes a node from the graph
+        // Returns true if it was successfully removed
+        pub fn remove_node(&mut self, node: &'a str) -> bool {
+            let mut out = false;
+            // Remove the entry in the table
+            if let Entry::Occupied(val) = self.adj_list.entry(node) {
+                val.remove_entry();
+                out = true;
+            }
+            // Get rid of the node in every other nodes list
+            for key in self.adj_list.iter_mut() {
+                // Remove the value in the list
+                if let Some(pos) = key.1.iter().position(|val| *val == node) {
+                    key.1.remove(pos) ;
+                    out = true;
+                }
+            }
+
+            return out;
+        }
+
+        // Removes an edge from the graph
+        pub fn remove_edge(&mut self, node1: &'a str, node2: &'a str) -> bool {
+            let mut out = false;
+
+            if let Some(val) = self.adj_list.get_mut(node1) {
+                if let Some(pos) = val.iter().position(|val| *val == node2) {
+                    val.remove(pos);
+                    out = true;
+                }
+            }
+
+            return out;
+        }
+    }
+}
--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,3 +1,13 @@
+use day_7::graph::Graph;
+
 fn main() {
     println!("Hello, world!");
+    let mut graph = Graph::new();
+    graph.insert_edge("hi", "there");
+    graph.insert_edge("hi", "general kenobi");
+
+    println!("{:?}", graph);
+    graph.remove_node("general kenobi");
+    graph.remove_edge("hi", "there");
+    println!("{:?}", graph);
 }
