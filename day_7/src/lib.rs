e8cc194fa2f1d835f3e41a432b103f136efbac5e
DarkenedDragon <eridor314@gmail.com>
2020-12-12 21:48:46 -0500

    Finished part 1 and modified Graph

--- day_7/src/lib.rs
+++ day_7/src/lib.rs
@@ -6,21 +6,33 @@ pub mod graph {
     use std::collections::hash_map::Entry;
 
     #[derive(Debug)]
-    pub struct Graph<'a> {
-        adj_list: HashMap<&'a str, Vec<&'a str>>
+    pub struct Graph {
+        adj_list: HashMap<String, Vec<Edge>>
     }
 
-    impl<'a> Graph<'a> {
-        pub fn new() -> Graph<'a> {
-            let adj_list: HashMap<&'a str, Vec<&'a str>> = HashMap::new();
+    pub struct Edge {
+        node: String,
+        weight: i32
+    }
+
+    impl PartialEq for Edge {
+        fn eq(&self, other: &Self) -> bool {
+            return self.node == other.node;
+        }
+    }
+    impl Eq for Edge {}
+
+    impl Graph {
+        pub fn new() -> Graph {
+            let adj_list: HashMap<String, Vec<Edge>> = HashMap::new();
             return Graph {adj_list};
         }
 
         // Inserts a node if not currently present. Does nothing with edges
-        pub fn insert_node(&mut self, node: &'a str) -> bool{
+        pub fn insert_node(&mut self, node: String) -> bool{
             let mut out = false;
-            if !self.adj_list.contains_key(node) {
-                out = match self.adj_list.insert(node, Vec::new()) {
+            if !self.adj_list.contains_key(&node) {
+                out = match self.adj_list.insert(node.to_owned(), Vec::new()) {
                     Some(_t) => false,
                     None => true
                 };
@@ -33,10 +45,10 @@ pub mod graph {
         // they will be added
         // This is a directed graph, so the node is placed node1 -> node2
         // Returns false if the edge was already present or could not be added
-        pub fn insert_edge(&mut self, node1: &'a str, node2: &'a str) -> bool {
+        pub fn insert_edge(&mut self, node1: String, node2: String, weight: i32) -> bool {
             let mut out = false;
             // Add node2 to the graph if not already present
-            if !self.adj_list.contains_key(node2) {
+            if !self.adj_list.contains_key(&node2) {
                 out = match self.adj_list.insert(node2, Vec::new()) {
                     Some(_t) => false,
                     None => true
@@ -45,8 +57,9 @@ pub mod graph {
             // Add node2 to node1's list
             // Will add node1 if not present
             let node = self.adj_list.entry(node1).or_insert(Vec::new());
-            if !node.contains(&node2) {
-                node.push(node2);
+            let edge = Edge {node: node2.to_owned(), weight};
+            if !node.contains(&edge) {
+                node.push(edge);
                 out = true;
             }
 
@@ -55,17 +68,17 @@ pub mod graph {
 
         // Removes a node from the graph
         // Returns true if it was successfully removed
-        pub fn remove_node(&mut self, node: &'a str) -> bool {
+        pub fn remove_node(&mut self, node: &str) -> bool {
             let mut out = false;
             // Remove the entry in the table
-            if let Entry::Occupied(val) = self.adj_list.entry(node) {
+            if let Entry::Occupied(val) = self.adj_list.entry(node.to_owned()) {
                 val.remove_entry();
                 out = true;
             }
             // Get rid of the node in every other nodes list
             for key in self.adj_list.iter_mut() {
                 // Remove the value in the list
-                if let Some(pos) = key.1.iter().position(|val| *val == node) {
+                if let Some(pos) = key.1.iter().position(|val| *val.node == node) {
                     key.1.remove(pos) ;
                     out = true;
                 }
@@ -75,17 +88,22 @@ pub mod graph {
         }
 
         // Removes an edge from the graph
-        pub fn remove_edge(&mut self, node1: &'a str, node2: &'a str) -> bool {
+        pub fn remove_edge(&mut self, node1: &str, node2: &str) -> bool {
             let mut out = false;
 
             if let Some(val) = self.adj_list.get_mut(node1) {
-                if let Some(pos) = val.iter().position(|val| *val == node2) {
+                if let Some(pos) = val.iter().position(|val| *val.node == node2) {
                     val.remove(pos);
                     out = true;
                 }
             }
 
             return out;
+        }
+
+        // Get the adjacent nodes
+        pub fn get_adjacent(&self, node: &str) -> Option<&Vec<Edge>> {
+            return self.adj_list.get(node);
         }
     }
 }
--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,13 +1,69 @@
 use day_7::graph::Graph;
+use std::fs;
+use std::collections::{VecDeque, HashMap};
+use std::collections::hash_map::Entry;
+use std::borrow::Borrow;
+
+/*
+Create a graph were each node is a bag, and each edge represents being contained within another
+directed graph: if A -> B, then A is contained within B
+Counter-intuitive, but allows us to use BFS to find all the bags that we are contained by
+ */
 
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input7.txt").expect("Failed to read input file");
     let mut graph = Graph::new();
-    graph.insert_edge("hi", "there");
-    graph.insert_edge("hi", "general kenobi");
 
-    println!("{:?}", graph);
-    graph.remove_node("general kenobi");
-    graph.remove_edge("hi", "there");
-    println!("{:?}", graph);
+    for line in input.lines() {
+        let words = line.split_ascii_whitespace().collect::<Vec<&str>>();
+        let name = words[0].to_owned() + " " + words[1];
+
+        // check if this bag contains any other bags
+        if words[4] == "no" {
+            graph.insert_node(name);
+        } else {
+            let mut i = 4;
+            while i < words.len() {
+                // Add the contained bags to the graph
+                let _weight = words[i];
+                let contained_name = words[i+1].to_owned() + " " +  words[i+2];
+                graph.insert_edge(contained_name, name.clone());
+
+                i += 4;
+            }
+        }
+    }
+
+    // BFS
+    let mut queue :VecDeque<&str>= VecDeque::new();
+    let mut checked: HashMap<&str, bool> = HashMap::new();
+    let start = "shiny gold";
+    queue.push_front(start);
+    checked.insert(start, false);
+
+    let mut count = -1; // accounts for the first node
+    while !queue.is_empty() {
+        if let Some(current) = queue.pop_back() {
+            // Check that we haven't already covered this node
+            if let Entry::Occupied(mut ocp) = checked.entry(current) {
+                if *ocp.get() == false {
+                    *ocp.get_mut() = true;
+                    // Add the others
+                    if let Some(adj) = graph.get_adjacent(current) {
+                        for node in adj {
+                            queue.push_front(node);
+                            // super hacky. Don't know why contains_key won't take a &str straight up
+                            if !checked.contains_key(node.borrow() as &str) {
+                                checked.insert(node, false);
+                            }
+                        }
+                    }
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    println!("There are {} colors contained in {}", count, start);
+
 }
